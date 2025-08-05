52e47039d78f7ba265aef8f8ac85567badcaea96
Sidarta Veloso <sidartaveloso@gmail.com>
2025-08-04 22:40:05 -0300

    chore: add sqlite3 to pnpm workspace and update Directus imports for AssetsService

--- src/utils/DirectusImports.ts
+++ src/utils/DirectusImports.ts
@@ -1,7 +1,8 @@
-export { ItemsService } from '@directus/api/dist/services/items';
-export type { AbstractServiceOptions } from '@directus/api/dist/types/services';
-export type { ApiExtensionContext } from '@directus/extensions/dist';
-export type { PrimaryKey } from '@directus/types';
 export { defineHook } from '@directus/extensions-sdk';
 export { readInnerFile } from '../utils/files';
 export type { Collection, Field, Relation, SchemaOverview } from '@directus/types';
+export type { AssetsService } from '@directus/api/dist/services/assets';
+export { ItemsService } from '@directus/api/dist/services/items';
+export type { AbstractServiceOptions } from '@directus/api/dist/types';
+export type { ApiExtensionContext } from '@directus/extensions';
+export type { PrimaryKey } from '@directus/types';
